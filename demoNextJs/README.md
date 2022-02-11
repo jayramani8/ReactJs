5f742ab799b975d55ef59ea8eab463704d86ee70
jayramani8 <70213053+jayramani8@users.noreply.github.com>
2022-02-11 19:38:36 +0530

    Add files via upload

---
+++ demoNextJs/README.md
@@ -0,0 +1,3 @@
+<h1>Event App</h1>
+<p>I deployed this project on VERCEL.</p>
+<h3>Go to <a href='https://event-app-gules.vercel.app/'>Event App</a></h3>
---
+++ demoNextJs/helpers/db-util.js
@@ -0,0 +1,25 @@
+import { MongoClient } from 'mongodb';
+
+const connectionString = "mongodb+srv://jayramani:99ByRvbpZXKwnJ9W@cluster0.1pjpa.mongodb.net/events?retryWrites=true&w=majority";
+
+export async function connectDatabase() {
+  const client = await MongoClient.connect(connectionString);
+
+  return client;
+}
+
+export async function insertDocument(client, collection, document) {
+  const db = client.db();
+
+  const result = await db.collection(collection).insertOne(document);
+
+  return result;
+}
+
+export async function getAllDocuments(client, collection, sort) {
+  const db = client.db();
+
+  const documents = await db.collection(collection).find().sort(sort).toArray();
+
+  return documents;
+}
